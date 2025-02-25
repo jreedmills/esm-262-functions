fd2cc3c74b68f3ad8a5df332665d858a92006271
jreedmills <jrmills@ucsb.edu>
2025-02-25 08:40:15 -0800

    add wind power function to repo

---
+++ R/wind_power_function.R
@@ -0,0 +1,34 @@
+# function for calculating wind power availability
+# author: Jackson Mills
+
+# inputs:
+# rho: air density (kg/m^3)
+# r: blade length (m)
+# v: wind speed (m/s)
+
+# output:
+# p: power (MW)
+
+wind_power <- function(rho, r, v){
+  
+  # error checking:
+  # wind speed must be greater than zero
+  ifelse(v < 0, stop("Wind speed must be greater than zero"), v)
+  
+  # air density must be greater than zero
+  ifelse(rho < 0, stop("Air density must be greater than zero"), rho)
+  
+  # if air density is greater than 2, issue a warning
+  ifelse(rho > 2, warning("Air density is unrealistically high"), rho)
+  
+  # area must be greater than zero
+  ifelse(r < 0, stop("Blade length must be greater than zero"), r)
+  
+  # define the power equation using the formula p = 0.5 * rho * A * v^3
+  # note: A = pi * r^2
+  p = 0.5 * rho * pi * r^2 * v^3
+  
+  # return the result, dividing by 10^6 to convert to megawatts
+  return(p/10^6)
+}
+
